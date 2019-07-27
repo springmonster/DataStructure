3e301fafdd6f51ca75bbb1ac1adc57dea9dfc61f
springmonster <charleskuanghc@163.com>
2019-07-27 21:24:29 +0800

    modify AVL fix some error

--- src/com/khch/datastructure/tree/avl/AVLTree.java
+++ src/com/khch/datastructure/tree/avl/AVLTree.java
@@ -1,6 +1,10 @@
 package com.khch.datastructure.tree.avl;
 
 public class AVLTree {
+    public static final int LEFT_HIGH = 1;
+    public static final int EQUAL_HIGH = 0;
+    public static final int RIGHT_HIGH = -1;
+
     private AVLNode root;
 
     public void insert(AVLNode avlNode) {
@@ -54,32 +58,32 @@ public class AVLTree {
 
         // 说明插入在了左孩子的左子树上，需要进行右旋
         if (needToRotateNode.balanceFactor == 1) {
-            father.balanceFactor = 0;
-            needToRotateNode.balanceFactor = 0;
+            father.balanceFactor = EQUAL_HIGH;
+            needToRotateNode.balanceFactor = EQUAL_HIGH;
             rightRotate(father);
         }
         // 说明插入在了左孩子的右子树上，需要双旋处理
         // 先进行左旋，再进行右旋
-        if (needToRotateNode.balanceFactor == -1) {
+        if (needToRotateNode.balanceFactor == RIGHT_HIGH) {
             AVLNode rightChild = needToRotateNode.right;
 
             switch (rightChild.balanceFactor) {
-                case 0:
-                    father.balanceFactor = 0;
-                    needToRotateNode.balanceFactor = 0;
+                case EQUAL_HIGH:
+                    father.balanceFactor = EQUAL_HIGH;
+                    needToRotateNode.balanceFactor = EQUAL_HIGH;
                     break;
-                case 1:
-                    needToRotateNode.balanceFactor = 0;
-                    father.balanceFactor = -1;
+                case LEFT_HIGH:
+                    needToRotateNode.balanceFactor = EQUAL_HIGH;
+                    father.balanceFactor = RIGHT_HIGH;
                     break;
-                case -1:
-                    needToRotateNode.balanceFactor = 1;
-                    father.balanceFactor = 0;
+                case RIGHT_HIGH:
+                    needToRotateNode.balanceFactor = LEFT_HIGH;
+                    father.balanceFactor = EQUAL_HIGH;
                     break;
                 default:
                     break;
             }
-            rightChild.balanceFactor = 0;
+            rightChild.balanceFactor = EQUAL_HIGH;
             father = leftRotate(father);
             father = rightRotate(father);
         }
@@ -96,33 +100,33 @@ public class AVLTree {
         AVLNode needToRotateNode = father.right;
 
         // 说明插入在了右孩子的右子树上，需要进行左旋
-        if (needToRotateNode.balanceFactor == -1) {
-            father.balanceFactor = 0;
-            needToRotateNode.balanceFactor = 0;
+        if (needToRotateNode.balanceFactor == RIGHT_HIGH) {
+            father.balanceFactor = EQUAL_HIGH;
+            needToRotateNode.balanceFactor = EQUAL_HIGH;
             leftRotate(father);
         }
         // 说明插入在了右孩子的左子树上，需要双旋处理
         // 先进行右旋，再进行左旋
-        if (needToRotateNode.balanceFactor == 1) {
+        if (needToRotateNode.balanceFactor == LEFT_HIGH) {
             AVLNode leftChild = needToRotateNode.left;
 
             switch (leftChild.balanceFactor) {
-                case 0:
-                    father.balanceFactor = 0;
-                    needToRotateNode.balanceFactor = 0;
+                case EQUAL_HIGH:
+                    father.balanceFactor = EQUAL_HIGH;
+                    needToRotateNode.balanceFactor = EQUAL_HIGH;
                     break;
-                case 1:
-                    needToRotateNode.balanceFactor = 0;
-                    father.balanceFactor = -1;
+                case LEFT_HIGH:
+                    needToRotateNode.balanceFactor = RIGHT_HIGH;
+                    father.balanceFactor = EQUAL_HIGH;
                     break;
-                case -1:
-                    needToRotateNode.balanceFactor = 1;
-                    father.balanceFactor = 0;
+                case RIGHT_HIGH:
+                    needToRotateNode.balanceFactor = EQUAL_HIGH;
+                    father.balanceFactor = LEFT_HIGH;
                     break;
                 default:
                     break;
             }
-            leftChild.balanceFactor = 0;
+            leftChild.balanceFactor = EQUAL_HIGH;
             father = rightRotate(father);
             father = leftRotate(father);
         }
