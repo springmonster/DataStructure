c47088c6a91b0f8164a13ec4cb1b4082a961c8fb
wfdanlover <wfdanlover@126.com>
2020-03-12 21:03:49 +0800

    add Class.forName

--- src/com/interview/reflect/Robot.java
+++ src/com/interview/reflect/Robot.java
@@ -1,6 +1,10 @@
 package com.interview.reflect;
 
 public class Robot {
+    static {
+        System.out.println("Hello Robot");
+    }
+
     private String name;
 
     private String sayHello(String tag) {
