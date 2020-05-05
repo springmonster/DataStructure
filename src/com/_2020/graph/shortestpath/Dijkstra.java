6ccfb88864a48c3caaf6675f16901de2de5eb264
charles.kuang <charles.kuang@homecreditcfc.cn>
2020-05-05 21:43:42 +0800

    add Dijkstra.java

--- src/com/_2020/graph/shortestpath/Dijkstra.java
+++ src/com/_2020/graph/shortestpath/Dijkstra.java
@@ -6,24 +6,31 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
- * 通过第1，2，3。。。N条边来查看
- * e[i][j] > e[i][k] + e[k][j]
+ *
  */
-public class FloydWarshall {
+public class Dijkstra {
     private static int[][] array;
+    // 一维数组记录源点到其他点的最短距离
+    private static int[] dis;
+    // 记录哪些点已经被访问过
+    private static int[] book;
     private static int nums;
     private static final int MAX_VALUE = 9999;
+    // 每次寻找的最小值
+    private static int min;
+    // 每次寻找的最小值的下标
+    private static int index;
 
     public static void main(String[] args) throws FileNotFoundException {
         initGraph();
         display(nums);
         System.out.println("Result as below:");
-        floydWallshall(nums);
-        display(nums);
+        dijkstra(nums);
+        displayResult();
     }
 
     private static void initGraph() throws FileNotFoundException {
-        File file = new File("floydwarshall");
+        File file = new File("dijkstra");
         Scanner scanner = new Scanner(file);
 
         nums = scanner.nextInt();
@@ -46,16 +53,32 @@ public class FloydWarshall {
             System.out.println(firstNum);
             System.out.println(secondNum);
             System.out.println(weight);
+
             array[firstNum][secondNum] = weight;
         }
+
+        dis = array[1];
+        book = new int[nums + 1];
+        book[0] = book[1] = 1;
     }
 
-    private static void floydWallshall(int nums) {
-        for (int k = 1; k <= nums; k++) {
-            for (int i = 1; i <= nums; i++) {
-                for (int j = 1; j <= nums; j++) {
-                    if (array[i][j] > array[i][k] + array[k][j]) {
-                        array[i][j] = array[i][k] + array[k][j];
+    private static void dijkstra(int nums) {
+        for (int i = 1; i <= nums; i++) {
+            // 首先找到最小的边，并且顶点未被访问过
+            min = MAX_VALUE;
+            for (int j = 1; j <= nums; j++) {
+                if (book[j] != 1 && dis[j] < min) {
+                    min = dis[j];
+                    index = j;
+                }
+            }
+            // 设置为访问过
+            book[index] = 1;
+            // 访问二维数组中的index的行，如果通过index这个当前选出的最小点，还能将距离进行缩短，那么就进行松弛
+            for (int v = 1; v <= nums; v++) {
+                if (array[index][v] != MAX_VALUE) {
+                    if (dis[v] > dis[index] + array[index][v]) {
+                        dis[v] = dis[index] + array[index][v];
                     }
                 }
             }
@@ -68,6 +91,12 @@ public class FloydWarshall {
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
+        }
+    }
+
+    private static void displayResult() {
+        for (int di : dis) {
+            System.out.print(di + " ");
         }
     }
 }
